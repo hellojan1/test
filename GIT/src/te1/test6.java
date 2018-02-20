07d2b06318b3ad4dbbda21242bcbdfb681e6680a
tmxld <tmxld@DESKTOP-4TMVELM>
2018-02-20 12:29:10 +0900

    333

--- GIT/src/te1/test6.java
+++ GIT/src/te1/test6.java
@@ -4,6 +4,7 @@ public class test6 {
 	 public static void main(String[] args) {
 		System.out.println("test666666");
 		System.out.println("1111111122222");
+		System.out.println("3333333333333");
 	}
 }
   
