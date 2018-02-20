4b1ee1b46f5cd03ee60370629c0e1ee99b3e149c
tmxld <tmxld@tmxld>
2018-02-20 14:18:57 +0900

    123

--- GIT/src/te1/test6.java
+++ GIT/src/te1/test6.java
@@ -5,6 +5,8 @@ public class test6 {
 			System.out.println("test666666");
 			System.out.println("1111111122222");
 			System.out.println("33333333333333");
+			
+			System.out.println("33333333333333");
 		}
 }
   
