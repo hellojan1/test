92e6e5520bf2f5119364dc5261d6662f9de2c86c
jong <jong@jong>
2018-02-20 12:35:48 +0900

    44

--- GIT/src/te1/test6.java
+++ GIT/src/te1/test6.java
@@ -1,24 +1,10 @@
 package te1;
 
 public class test6 {
- public static void main(String[] args) {
-	System.out.println("test666666");
-	System.out.println("1111111122222");
-
-	
-	
-	try {
-		System.out.println("1111111122222");
-	} catch (Exception e) {
-		// TODO: handle exception
-	}
-	
-	
-	
-	
-	
-	
-	
-}
+	 public static void main(String[] args) {
+			System.out.println("test666666");
+			System.out.println("1111111122222");
+			System.out.println("33333333333333");
+		}
 }
   
