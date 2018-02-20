263c61e3ec44b1cbd67f292ab67af6eebe45f519
jong <jong@jong>
2018-02-20 12:21:18 +0900

    # WARNING: head commit changed in the meantime
    dd

--- GIT/src/te1/test6.java
+++ GIT/src/te1/test6.java
@@ -4,6 +4,15 @@ public class test6 {
  public static void main(String[] args) {
 	System.out.println("test666666");
 	System.out.println("1111111122222");
+
+	
+	
+	try {
+		System.out.println("1111111122222");
+	} catch (Exception e) {
+		// TODO: handle exception
+	}
+	
 	
 	
 	
