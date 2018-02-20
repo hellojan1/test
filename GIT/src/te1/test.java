1b743de2094b72aa8d224830d6cba0bdab3941d5
JYH <JYH@DESKTOP-UCEIJ2U>
2018-02-20 18:52:13 +0900

    test1 update

--- GIT/src/te1/test.java
+++ GIT/src/te1/test.java
@@ -6,8 +6,8 @@ public class test {
 		System.out.println("111");
 		System.out.println("222");
 		System.out.println("333");
-		System.out.println("333");
-
+		System.out.println("444");
+		System.out.println("555");
+		System.out.println("666");
 	}
-
 }
