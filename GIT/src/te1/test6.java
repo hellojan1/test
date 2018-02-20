e2c3194707716dc933b138970bfa37beb688d01f
tmxld <tmxld@DESKTOP-4TMVELM>
2018-02-20 12:27:52 +0900

    k

--- GIT/src/te1/test6.java
+++ GIT/src/te1/test6.java
@@ -1,27 +1,9 @@
 package te1;
 
 public class test6 {
- public static void main(String[] args) {
-	System.out.println("test666666");
-	System.out.println("1111111122222");
-	System.out.println("1111111122222");
-	System.out.println("1111111122222");
-	System.out.println("1111111122222");
-
-	
-	
-	try {
+	 public static void main(String[] args) {
+		System.out.println("test666666");
 		System.out.println("1111111122222");
-	} catch (Exception e) {
-		// TODO: handle exception
 	}
-	
-	
-	
-	
-	
-	
-	
-}
 }
   
