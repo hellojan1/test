55546cbc555b0701c39deece2a08c03d4f640e91
tmxld <tmxld@DESKTOP-4TMVELM>
2018-02-20 12:16:52 +0900

    jj

--- GIT/src/te1/test6.java
+++ GIT/src/te1/test6.java
@@ -3,7 +3,9 @@ package te1;
 public class test6 {
  public static void main(String[] args) {
 	System.out.println("test666666");
-	System.out.println("11111111");
+	System.out.println("1111111122222");
+	
+	
 }
 }
   
