206a1eefcd79a5be9eabae6d285908a1d60834a9
hellojan1 <36619059+hellojan1@users.noreply.github.com>
2018-02-20 11:13:56 +0900

    Add files via upload

---
+++ GIT/src/te1/test1.java
@@ -0,0 +1,13 @@
+package te1;
+
+public class test {
+
+	public static void main(String[] args) {
+		System.out.println("111");
+		System.out.println("222");
+		System.out.println("333");
+		System.out.println("333");
+
+	}
+
+}
